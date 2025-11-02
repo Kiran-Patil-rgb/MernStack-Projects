e1fc0ed8bbf11de22d6170fc953f36cd4c6ae73e
[Kiran-Patil-rgb] <[kiranpatil5225@gmail.com]>
2025-11-02 18:40:17 +0530

    project file

---
+++ server/models/userSchema.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+
+let addressObject = {
+    street: "", city: "", state: "", country: "", pincode: ""
+}
+
+let emailObject = {
+    userEmail: "", verified: false
+}
+
+let userShcema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: Object,
+        require: true,
+        default: emailObject
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    address: {
+        type: Object,
+        require: true,
+        default: addressObject
+    },
+    dob: {
+        type: String,
+        require: true
+    },
+    qualifications: {
+        type: String,
+        default: ""
+    },
+    documents: {
+        type: Array,
+        default: []
+    },
+    appliedJobs: {
+        type: Array,
+        default: []
+    },
+    timeStamp: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+userShcema.pre("save", async function () {
+    try {
+        console.log("user password is :", this.password)
+        this.password = await bcrypt.hash(this.password, 10)
+        console.log("password hased and saved !")
+    } catch (err) {
+        console.log("error in pre method : ", err)
+        throw err
+    }
+})
+
+let userModel = new mongoose.model("users", userShcema)
+
+export { userModel }
---
+++ server/server.js
@@ -0,0 +1,38 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import "./database/conn.js"
+
+import { userRouter } from "./routers/userRouter.js"
+
+
+dotenv.config({ path: "./config.env" })
+
+const app = express()
+
+let port = process.env.PORT || 5012
+
+app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const corsOptions = {
+  origin: "*",  
+  methods: "*",
+};
+
+app.use(cors(corsOptions))
+
+// routers
+
+app.use("/user", userRouter)
+
+
+app.use((req, res) => {
+    console.log("user trying to access invalid route !")
+    res.status(404).json({ message: "content/route not found !" })
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port} !`)
+})
